6a6b5bb53551e1ed6b8b3fd26c2f6cd16d0c2987
Kimmer Wang <Kimmer_Wang@resourcepro.com.cn>
2024-08-14 19:32:39 +0800

    AsyncPractice

---
+++ KimmerPractice2/AsyncPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AsyncPractice
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string fileName = @"C:\Users\kimmer_wang\Downloads\KimmerTest.txt";
+
+            //File.WriteAllText(fileName, "Hello");
+            //string s = File.ReadAllText(fileName);
+
+            File.WriteAllTextAsync(fileName, "Hello1234");
+            File.ReadAllTextAsync(fileName);
+
+            await File.WriteAllTextAsync(fileName, "Hello");
+            string s = await File.ReadAllTextAsync(fileName);
+
+            Console.WriteLine(s);
+
+            Console.ReadLine();
+        }
+    }
+}
--- KimmerPractice2/DotnetCorePractice/Program.cs
+++ KimmerPractice2/DotnetCorePractice/Program.cs
@@ -4,35 +4,20 @@ namespace DotnetCorePractice
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             #region - .net standard & .net core & .net framework
-            //Console.WriteLine(typeof(FileStream).Assembly.Location);
+            Console.WriteLine(typeof(FileStream).Assembly.Location);
 
-            //FileStreamPeactice practice = new FileStreamPeactice();
-            //Console.WriteLine(practice.GetType().Assembly.Location);
+            FileStreamPeactice practice = new FileStreamPeactice();
+            Console.WriteLine(practice.GetType().Assembly.Location);
 
-            //practice.WriteTypeLocation();
+            practice.WriteTypeLocation();
 
-            //Console.WriteLine(typeof(FileStreamPeactice).Assembly.Location);
-
-            //Console.ReadLine();
-            #endregion
-
-            string fileName = @"C:\Users\kimmer_wang\Downloads\KimmerTest.txt";
-
-            //File.WriteAllText(fileName, "Hello");
-            //string s = File.ReadAllText(fileName);
-
-            File.WriteAllTextAsync(fileName, "Hello1234");
-            File.ReadAllTextAsync(fileName);
-
-            await File.WriteAllTextAsync(fileName, "Hello");
-            string s = await File.ReadAllTextAsync(fileName);
-
-            Console.WriteLine(s);
+            Console.WriteLine(typeof(FileStreamPeactice).Assembly.Location);
 
             Console.ReadLine();
+            #endregion
         }
     }
 }
