ed67b2dc72ae95ebbcafb2650e5f7d1d210d71a4
Kimmer Wang <Kimmer_Wang@resourcepro.com.cn>
2024-08-14 19:22:20 +0800

    async await practice

--- KimmerPractice2/DotnetCorePractice/Program.cs
+++ KimmerPractice2/DotnetCorePractice/Program.cs
@@ -4,16 +4,33 @@ namespace DotnetCorePractice
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine(typeof(FileStream).Assembly.Location);
+            #region - .net standard & .net core & .net framework
+            //Console.WriteLine(typeof(FileStream).Assembly.Location);
 
-            FileStreamPeactice practice = new FileStreamPeactice();
-            Console.WriteLine(practice.GetType().Assembly.Location);
+            //FileStreamPeactice practice = new FileStreamPeactice();
+            //Console.WriteLine(practice.GetType().Assembly.Location);
 
-            practice.WriteTypeLocation();
+            //practice.WriteTypeLocation();
 
-            Console.WriteLine(typeof(FileStreamPeactice).Assembly.Location);
+            //Console.WriteLine(typeof(FileStreamPeactice).Assembly.Location);
+
+            //Console.ReadLine();
+            #endregion
+
+            string fileName = @"C:\Users\kimmer_wang\Downloads\KimmerTest.txt";
+
+            //File.WriteAllText(fileName, "Hello");
+            //string s = File.ReadAllText(fileName);
+
+            File.WriteAllTextAsync(fileName, "Hello1234");
+            File.ReadAllTextAsync(fileName);
+
+            await File.WriteAllTextAsync(fileName, "Hello");
+            string s = await File.ReadAllTextAsync(fileName);
+
+            Console.WriteLine(s);
 
             Console.ReadLine();
         }
