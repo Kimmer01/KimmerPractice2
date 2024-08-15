7c66d8c1a2e17e8d9601f082357ef10bfd4c12eb
Kimmer Wang <Kimmer_Wang@resourcepro.com.cn>
2024-08-15 20:14:30 +0800

    TestAwaitThreadAsync

--- KimmerPractice2/AsyncPractice/Program.cs
+++ KimmerPractice2/AsyncPractice/Program.cs
@@ -21,15 +21,88 @@
             //Console.ReadLine();
             #endregion
 
-            await DownloadHtmlToFile(@"https://www.baidu.com/",
-                @"C:\Users\kimmer_wang\Downloads\KimmerTest.txt");
+            #region - async & await & .Wait() & .Result
+            //DownloadHtmlToFile(@"https://www.baidu.com/", @"C:\Users\kimmer_wang\Downloads\KimmerTest.txt");
 
-            int length = await DownloadHtmlToFileAndGetContentLength(@"https://www.baidu.com/",
-                @"C:\Users\kimmer_wang\Downloads\KimmerTest.txt");
-            Console.WriteLine(length);
+            //await DownloadHtmlToFileAsync(@"https://www.baidu.com/",
+            //    @"C:\Users\kimmer_wang\Downloads\KimmerTest.txt");
+
+            //int length = await DownloadHtmlToFileAndGetContentLengthAsync(@"https://www.baidu.com/",
+            //    @"C:\Users\kimmer_wang\Downloads\KimmerTest.txt");
+            //Console.WriteLine(length);
+            #endregion
+
+            //await TestAwaitThreadAsync(@"https://www.baidu.com/", @"C:\Users\kimmer_wang\Downloads\KimmerTest.txt","Test");
+
+            Console.WriteLine($"Before threadId: {Thread.CurrentThread.ManagedThreadId}");
+            await TestAwaitThreadAsync(5000);
+            Console.WriteLine($"After threadId: {Thread.CurrentThread.ManagedThreadId}");
         }
 
-        static async Task DownloadHtmlToFile(string url, string fileName)
+        static async Task<double> TestAwaitThreadAsync(int times)
+        {
+            //Console.WriteLine($"{nameof(TestAwaitThreadAsync)} threadId: {Thread.CurrentThread.ManagedThreadId}");
+            //double result = 0;
+            //Random rand = new Random();
+            //for (int n = 0; n < times; n++)
+            //{
+            //    result += rand.NextDouble();
+            //}
+
+            //return result;
+
+            return await Task.Run(() =>
+            {
+                Console.WriteLine($"{nameof(TestAwaitThreadAsync)} threadId: {Thread.CurrentThread.ManagedThreadId}");
+                double result = 0;
+                Random rand = new Random();
+                for (int n = 0; n < times; n++)
+                {
+                    result += rand.NextDouble();
+                }
+
+                return result;
+            });
+        }
+
+        static async Task<string> TestAwaitThreadAsync(string url, string fileName, string fileContent)
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+
+            using (HttpClient client = new HttpClient())
+            {
+                string content = await client.GetStringAsync(url);
+            }
+
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+
+            await File.WriteAllTextAsync(fileName, fileContent);
+
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+
+            string fileContent2 = await File.ReadAllTextAsync(fileName);
+
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+
+            return fileContent2;
+        }
+
+        /// <summary>
+        /// If need call async method inside a non-async method, 
+        /// use .Wait() if async method no return value
+        /// use .Result if async method have return value
+        /// </summary>
+        static void DownloadHtmlToFile(string url, string fileName)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string content = client.GetStringAsync(url).Result;
+
+                File.WriteAllTextAsync(fileName, content).Wait();
+            }
+        }
+
+        static async Task DownloadHtmlToFileAsync(string url, string fileName)
         {
             using (HttpClient client = new HttpClient())
             {
@@ -39,7 +112,7 @@
             }
         }
 
-        static async Task<int> DownloadHtmlToFileAndGetContentLength(string url, string fileName)
+        static async Task<int> DownloadHtmlToFileAndGetContentLengthAsync(string url, string fileName)
         {
             using (HttpClient client = new HttpClient())
             {
